dfc6963f788eee871480abdb31d982f051e4caa0
Namunaacharya <namunaacharya92@gmail.com>
2025-05-08 11:55:16 +0545

    scrub

---
+++ Scrub/scrub.py
@@ -0,0 +1,15 @@
+def scrub_import():
+        from pyspark.sql import SparkSession
+
+        spark = SparkSession.builder.appName('python').config("spark.driver.memory", "4g").getOrCreate()
+        spark
+
+        rate_file = spark.read.option('multiline','True').json('/home/namuna-acharya/Desktop/zakipoint/Zaki-ETL-Task3/files/network_file.json')
+        provider_file = spark.read.option('multiline','True').json('/home/namuna-acharya/Desktop/zakipoint/Zaki-ETL-Task3/files/provider_file.json')
+
+        rate_file.printSchema()
+
+        provider_file.printSchema()
+
+if __name__ == "__main__":
+        scrub_import()
