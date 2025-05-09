2f78e6a3f13902aacb3905ccc4ba691abc4de1e3
Namunaacharya <namunaacharya92@gmail.com>
2025-05-09 11:44:58 +0545

    main file

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from Load import load
 def main():
     zip_path = sys.argv[1]
 
-    extract_path = extract.extract_import(zip_path)
-    scrub_path = scrub.scrub_import(extract_path)
-    load.load_import(scrub_path)
+    in_path,prov_path = extract.extract_import(zip_path)
+    rate_path,provider_path = scrub.scrub_import(in_path,prov_path)
+    load.load_import(rate_path,provider_path)
 
 if __name__ == "__main__":
     main()
