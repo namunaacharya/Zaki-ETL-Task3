bd1de0ead3996ef2d591c2b03911cb51eb86ca6d
Namunaacharya <namunaacharya92@gmail.com>
2025-05-09 10:05:04 +0545

    extract updated

--- Extract/extract.py
+++ Extract/extract.py
@@ -1,28 +1,32 @@
-def extract_import():
-        import zipfile
-        import ijson
-        import json
-        from decimal import Decimal
+import zipfile
+import ijson
+import json
+import os
+from decimal import Decimal
 
-        def con_decimal(obj):
-            if isinstance(obj, Decimal):
-                return float(obj)
-            raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+def extract_import(zip_path):
+    def con_decimal(obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+    
+    output_path = os.path.join(os.getcwd(),'output_file')
+    os.makedirs(output_path, exist_ok=True)
 
-        zip_file = "/home/namuna-acharya/Desktop/zakipoint/Zaki-ETL-Task3/files/MagnaCarePPO_In-Network.zip"
+    in_path = os.path.join(output_path, 'in_network.json')
+    prov_path = os.path.join(output_path, 'provider_ref.json')
 
-        with open('/home/namuna-acharya/Desktop/zakipoint/Zaki-ETL-Task3/files/network_file.json','w') as in_file, \
-            open('/home/namuna-acharya/Desktop/zakipoint/Zaki-ETL-Task3/files/provider_file.json','w') as prov_file:
- 
-             with zipfile.ZipFile(zip_file, 'r') as file:
-                for data in file.namelist():
-                    with file.open(data, 'r') as f:
-                        for item in ijson.items(f, 'in_network.item'):
-                            in_file.write(json.dumps(item, default=con_decimal) + '\n')
+    with zipfile.ZipFile(zip_path, 'r') as z:
+        for data in z.namelist():
+            with z.open(data, 'r') as f:
+                with open(in_path,'w') as in_file:
+                    for item in ijson.items(f, 'in_network.item'):
+                        in_file.write(json.dumps(item, default=con_decimal) + '\n')
 
-                        f.seek(0)
+                f.seek(0)
 
-                        for item in ijson.items(f, 'provider_references.item'):
-                            prov_file.write(json.dumps(item, default=con_decimal) + '\n')
-                    
+                with open(prov_path,'w') as prov_file:
+                    for item in ijson.items(f, 'provider_references.item'):
+                        prov_file.write(json.dumps(item,default=con_decimal) + '\n')
 
+    return in_path,prov_path
