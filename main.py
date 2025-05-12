56a090373de51cd4255266cb3e1dba247cd87ad5
Namunaacharya <namunaacharya92@gmail.com>
2025-05-12 14:23:40 +0545

    parse

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import sys
+# import sys
+import argparse
 from Extract import extract
 from Scrub import scrub
 from Load import load
 
 def main():
-    zip_path = sys.argv[1]
+    # zip_path = sys.argv[1]
+    parser = argparse.ArgumentParser(description="ETL processing using ZIP file.")
+    parser.add_argument("--zip_path",required=True, help="ZIP file path")
 
-    in_path,prov_path = extract.extract_import(zip_path)
+    args = parser.parse_args()
+
+    in_path,prov_path = extract.extract_import(args.zip_path)
     rate_path,provider_path = scrub.scrub_import(in_path,prov_path)
     load.load_import(rate_path,provider_path)
 
