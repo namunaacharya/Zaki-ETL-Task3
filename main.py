32665dbdd01b848a52b33ddb8d8421905297f4c6
Namunaacharya <namunaacharya92@gmail.com>
2025-05-14 14:04:28 +0545

    logging

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-# import sys
-import argparse
-from Extract import extract
-from Scrub import scrub
-from Load import load
+import logging
+from etl import ETL
 
 def main():
-    # zip_path = sys.argv[1]
-    parser = argparse.ArgumentParser(description="ETL processing using ZIP file.")
-    parser.add_argument("--zip_path",required=True, help="ZIP file path")
-
-    args = parser.parse_args()
-
-    in_path,prov_path = extract.extract_import(args.zip_path)
-    rate_path,provider_path = scrub.scrub_import(in_path,prov_path)
-    load.load_import(rate_path,provider_path)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
+    logger = logging.getLogger("ETLLogger")
+    etl = ETL()
+    etl.execute(logger)
 
 if __name__ == "__main__":
     main()
+
+
+
