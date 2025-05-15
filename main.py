715da408503613b5b4d94dbcdf54d4e3953102df
Namunaacharya <namunaacharya92@gmail.com>
2025-05-15 09:57:36 +0545

    update

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import argparse
 import logging
 from etl import ETL
+from api import API
 
 def main():
+    parser = argparse.ArgumentParser(description="ETL processing using ZIP file.")
+    parser.add_argument("--zip_path", required=True, help="ZIP file path")
+
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
     logger = logging.getLogger("ETLLogger")
     etl = ETL()
-    etl.execute(logger)
+    etl.execute(args,logger)
 
 if __name__ == "__main__":
     main()
