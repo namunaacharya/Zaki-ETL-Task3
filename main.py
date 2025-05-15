7acaaa563a54ccdc392b984c582d8469ca9100ed
Namunaacharya <namunaacharya92@gmail.com>
2025-05-15 14:55:47 +0545

    update

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import argparse
 import logging
 from etl import ETL
-from api import API
+from api import as_sort, des_sort
 
 def main():
-    parser = argparse.ArgumentParser(description="ETL processing using ZIP file.")
-    parser.add_argument("--zip_path", required=True, help="ZIP file path")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
+    logger = logging.getLogger("Logger")
+
+    parser = argparse.ArgumentParser(description="ETL processing or API planet sorting")
+    parser.add_argument('--zip_path', help='ZIP file path for ETL')
+    parser.add_argument('--apiA',action='store_true', help="Enter 'apiA' or 'apiD' for planet sorting")
+    parser.add_argument('--apiD',action='store_true', help="Enter 'apiA' or 'apiD' for planet sorting")
 
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
-    logger = logging.getLogger("ETLLogger")
-    etl = ETL()
-    etl.execute(args,logger)
+    if args.zip_path:
+        etl = ETL()
+        etl.execute(args,logger)
+    elif args.apiA:
+        as_sort(logger)
+    elif args.apiD:
+        des_sort(logger)
+    else:
+        logger.info("Either --zip_path for ETL process or --apiA, --apiD for planet sorting")
 
 if __name__ == "__main__":
     main()
-
-
-
