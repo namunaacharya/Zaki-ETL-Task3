231646519fc8b41bae0a9ec4eff40fd27ff43735
Namunaacharya <namunaacharya92@gmail.com>
2025-05-16 14:34:44 +0545

    prov

--- Scrub/scrub.py
+++ Scrub/scrub.py
@@ -1,13 +1,14 @@
-from pyspark.sql.functions import explode,col,expr,when
-from pyspark.sql.types import ArrayType, IntegerType, ShortType
+from pyspark.sql.functions import explode,col,expr,when,array,lit,concat
+from pyspark.sql.types import ArrayType, IntegerType, ShortType, LongType
 
-def scrub_import(in_path,prov_path,etl,logger):
+def scrub_import(in_path,prov_path,pro_path,etl,logger):
     logger.info("Starting transform process")
     
     spark = etl.spark
 
     rate_file = spark.read.json(in_path)
     provider_file = spark.read.json(prov_path)
+    p_file = spark.read.json(pro_path)
 
     rate_file.printSchema()
     provider_file.printSchema()
@@ -28,10 +29,34 @@ def scrub_import(in_path,prov_path,etl,logger):
                         when(col("tin_type") == "ein", 1)
                         .when(col("tin_type") == "npi", 2))
     provider_cleaned = provider_replaced.withColumn('tin', expr("REPLACE(tin, '-', '')"))
-
-    # Cast the datatype
     provider_prov = provider_cleaned.withColumn("provider_group_id", col("provider_group_id").cast(IntegerType()))
     provider_cast = provider_prov.withColumn("tin_type", col("tin_type").cast(ShortType()))
+
+    #New provider file
+    p_l = p_file.select("*",col('loc.lat').alias('lat'), 
+                            col('loc.lon').alias('lon') )
+
+    p_loc = p_l.drop("loc")
+
+    p_concat = p_loc.withColumn("provider_full_name", concat(col("provider_first_name"), lit(" "), col("provider_middle_name"), lit(" "),col("provider_last_name")))
+    p_merge = p_concat.withColumn("prv_taxonomy",array("prv_taxonomy_1_code", "prv_taxonomy_2_code", "prv_taxonomy_3_code")) \
+    .withColumn("prv_specialty",array("prv_specialty_1_desc", "prv_specialty_2_desc", "prv_specialty_3_desc"))
+    p_drop = p_merge.drop("prv_fax","prv_type_desc","provider_first_name","provider_last_name","provider_middle_name","provider_name_prefix_text",
+                        ("prv_taxonomy_1_code"),("prv_taxonomy_2_code"),("prv_taxonomy_3_code"),("prv_specialty_1_desc"),("prv_specialty_2_desc"),("prv_specialty_3_desc"))
+
+    p_map = p_drop.withColumn("prv_type_code",
+                            when(col("prv_type_code") == "P", 1)
+                            .when(col("prv_type_code") == "F", 2))
+
+    p_cast = p_map.withColumn("prv_type_code", col("prv_type_code").cast(IntegerType())) \
+                .withColumn("npi", col("npi").cast(LongType()))
+    
+    #Join two provider table
+    
+    n_provider_table = provider_cast.join(p_cast,provider_cast.npi ==  p_cast.npi,"inner") \
+                .drop(p_cast.npi)
+    
+    n_provider_table.show()
 
     # Flatten rate file
     rate_df = rate_file.withColumn("rates", explode("negotiated_rates"))
@@ -62,13 +87,12 @@ def scrub_import(in_path,prov_path,etl,logger):
     rate_cast = rate_prov.withColumn("service_code",col("service_code").cast(ArrayType(IntegerType())))
    
     rate_cast.printSchema()   
-    provider_cast.printSchema()
+    n_provider_table.printSchema()
     
-    rate_path = "output_file/rate_data.parquet"
-    provider_path = "output_file/provider_data.parquet"
+    rate_path = "output_file/rate_table.parquet"
+    provider_path = "output_file/provider_table.parquet"
 
     rate_cast.write.mode("overwrite").parquet(rate_path)
-    provider_cast.write.mode("overwrite").parquet(provider_path)
-
+    n_provider_table.write.mode("overwrite").parquet(provider_path)
 
     return rate_path,provider_path
