bbac0c2da310808d1912e4377f1408595b42f114
Namunaacharya <namunaacharya92@gmail.com>
2025-05-20 17:51:48 +0545

    edit

--- Scrub/scrub_nrpr.py
+++ Scrub/scrub_nrpr.py
@@ -1,11 +1,11 @@
 
-def scrub_import(nrpr_path,pro_path,etl,logger):
+def scrub_import(nrpr_path,prov,bill,etl,logger):
     logger.info("Starting transform process")
 
     spark = etl.spark
 
     nrpr_file = spark.read.json(nrpr_path)
-    pro_file = spark.read.parquet(pro_path)
+    pro_file = spark.read.parquet(prov)
 
     nrpr_file.printSchema()
     pro_file.printSchema()
