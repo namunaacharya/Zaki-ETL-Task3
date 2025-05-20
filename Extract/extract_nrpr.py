b9435ac7a4cff9d98c6085fadf2db12c029dab65
Namunaacharya <namunaacharya92@gmail.com>
2025-05-20 17:43:06 +0545

    extract

--- Extract/extract_nrpr.py
+++ Extract/extract_nrpr.py
@@ -6,7 +6,7 @@ import os
 from io import BytesIO
 from decimal import Decimal
 
-def extract_import(zip_path,prov_path, logger):
+def extract_import(zip_path, logger):
     logger.info("Starting extract process")
 
     def con_decimal(obj):
@@ -18,23 +18,15 @@ def extract_import(zip_path,prov_path, logger):
     os.makedirs(output_path, exist_ok=True)
 
     nrpr_path = os.path.join(output_path, 'nrpr.json')
-    pro_path =  os.path.join(output_path, 'provider_details')
 
     with zipfile.ZipFile(zip_path, 'r') as outer_zip:
         for name in outer_zip.namelist():
             if name.endswith('.json.gz'):
                 with outer_zip.open(name) as new_file:
-                    with gzip.open(BytesIO(new_file.read()), 'rt', encoding='utf-8') as f:
+                    with gzip.open(BytesIO(new_file.read()), 'r', encoding='utf-8') as f:
                         with open(nrpr_path, 'w') as nrpr_file:
                             for item in ijson.items(f, 'in_network.item'):
                                 nrpr_file.write(json.dumps(item, default=con_decimal) + '\n')
 
-    with zipfile.ZipFile(prov_path, 'r') as z:
-        for file_name in z.namelist():
-            if file_name.endswith('.parquet'):
-                with z.open(file_name) as parquet_file:
-                    with open(pro_path, 'wb') as f_out:
-                        f_out.write(parquet_file.read())
-
     logger.info("File extracted successfully")
-    return nrpr_path,pro_path
+    return nrpr_path
