94c764d8b8a930e73e51811f8c2ddf49d6b575fa
Namunaacharya <namunaacharya92@gmail.com>
2025-05-21 16:12:08 +0545

    in

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main():
     logger = logging.getLogger("Logger")
 
     parser = argparse.ArgumentParser(description="ETL processing.")
-    parser.add_argument('--zip', required=True, help='ZIP file path for ETL')
+    parser.add_argument('--input', required=True, help='Input folder path for ETL')
     parser.add_argument('--prov', required=True, help='Provider detail zipfile path')
-    parser.add_argument('--bill', required=True, help='Billing detail zipfile path')
+    parser.add_argument('--bill', help='Billing detail zipfile path')
 
     args = parser.parse_args()
 
