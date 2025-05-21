84a07de676bc75e6336a48990c4ea4597654796c
Namunaacharya <namunaacharya92@gmail.com>
2025-05-21 16:13:41 +0545

    update

--- Extract/extract_nrpr.py
+++ Extract/extract_nrpr.py
@@ -1,32 +1,23 @@
-import zipfile
 import gzip
-import ijson
 import json
 import os
-from io import BytesIO
-from decimal import Decimal
 
-def extract_import(zip, logger):
+def extract_import(input, logger):
     logger.info("Starting extract process")
-
-    def con_decimal(obj):
-        if isinstance(obj, Decimal):
-            return float(obj)
-        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
 
     output_path = os.path.join(os.getcwd(), 'output_files')
     os.makedirs(output_path, exist_ok=True)
 
-    nrpr_path = os.path.join(output_path, 'nrpr.json')
+    nrpr_file = os.path.join(output_path, 'nrpr.json')
 
-    with zipfile.ZipFile(zip, 'r') as outer_zip:
-        for name in outer_zip.namelist():
-            if name.endswith('.json.gz'):
-                with outer_zip.open(name) as new_file:
-                    with gzip.open(BytesIO(new_file.read()), 'rt', encoding='utf-8') as f:
-                        with open(nrpr_path, 'w') as nrpr_file:
-                            for item in ijson.items(f, 'in_network.item'):
-                                nrpr_file.write(json.dumps(item, default=con_decimal) + '\n')
+    for files in os.listdir(input):
+        if files.endswith('.json.gz'):
+            file_path = os.path.join(input, files)
+            with gzip.open(file_path, 'rt', encoding='utf-8') as f:
+                        item = json.load(f)
+                        with open(nrpr_file, 'w', encoding='utf-8') as nrpr:
+                            json.dump(item, nrpr)
 
     logger.info("File extracted successfully")
-    return nrpr_path
+    return nrpr_file
+
