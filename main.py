8de2f5518ba54981f965c05c024c7ad729db0362
Namunaacharya <namunaacharya92@gmail.com>
2025-05-22 15:08:51 +0545

    new

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from etl_nrpr import ETL
 
 def main():
     logging.basicConfig(filename="ETLnrpr.log",encoding='utf-8',level=logging.INFO,
-                        format='%(asctime)s : %(message)s')
+                        format='%(asctime)s %(levelname)s: %(message)s')
     logger = logging.getLogger("Logger")
 
     parser = argparse.ArgumentParser(description="ETL processing.")
-    parser.add_argument('--input', required=True, help='Input folder path for ETL')
-    parser.add_argument('--prov', required=True, help='Provider detail zipfile path')
-    parser.add_argument('--bill', help='Billing detail zipfile path')
+    parser.add_argument('--input' , help='Input folder path for ETL')
+    parser.add_argument('--prov', help='Provider detail zipfile path')
 
     args = parser.parse_args()
 
