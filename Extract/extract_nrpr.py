4a74c29a204c9efb84788ad8f40a12db9c0d080f
Namunaacharya <namunaacharya92@gmail.com>
2025-05-23 16:32:36 +0545

    try except

--- Extract/extract_nrpr.py
+++ Extract/extract_nrpr.py
@@ -1,23 +1,31 @@
 import gzip
 import json
+import ijson
+from decimal import Decimal 
 import os
 
 def extract_import(input, logger):
     logger.info("Starting extract process")
 
+    def con_decimal(obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
     output_path = os.path.join(os.getcwd(), 'output_files')
     os.makedirs(output_path, exist_ok=True)
-
     nrpr_file = os.path.join(output_path, 'nrpr.json')
 
-    for files in os.listdir(input):
-        if files.endswith('.json.gz'):
-            file_path = os.path.join(input, files)
-            with gzip.open(file_path, 'rt') as f:
-                        item = json.load(f)
-                        with open(nrpr_file, 'w') as nrpr:
-                            json.dump(item, nrpr)
+    for file in os.listdir(input):
+        file_path = os.path.join(input, file)
+        if file.endswith('.json.gz'):
+            try:
+                with gzip.open(file_path, 'rt') as f :
+                    with open(nrpr_file, 'w') as nrpr:
+                        for item in ijson.items(f, ''):
+                            nrpr.write(json.dumps(item,default=con_decimal) + '\n')
+            except Exception as e:
+                logger.info(e)
 
     logger.info("File extracted successfully")
     return nrpr_file
-
